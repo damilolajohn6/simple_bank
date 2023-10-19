4f67969675cd43495935ca5243a6e809e0d66180
damilolajohn6 <damilolajohn622@gmail.com>
2023-10-19 14:19:08 +0100

    setting up using supabase

---
+++ backend/config/supabaseConfig.js
@@ -0,0 +1,11 @@
+// const { createClient } = require('@supabase/supabase-js');
+
+// const supabase = createClient('YOUR_SUPABASE_PROJECT_URL', 'YOUR_SUPABASE_API_KEY');
+import { createClient } from "@supabase/supabase-js";
+require('dotenv').config();
+
+const supabaseUrl = "https://paqatbrjdbtmcnnhuczm.supabase.co";
+const supabaseKey = process.env.SUPABASE_KEY;
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+module.exports = supabase;
